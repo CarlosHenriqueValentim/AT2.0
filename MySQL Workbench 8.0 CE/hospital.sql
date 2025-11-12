8cadd969af59df8270b2c04b0ba66d53c47accf3
Carlos Henrique Valentim Silva <rikcar22@gmail.com>
2025-11-12 00:43:45 -0300

    Add Script

---
+++ MySQL Workbench 8.0 CE/hospital.sql
@@ -0,0 +1,11 @@
+drop database if exists hospital;
+create database if not exists hospital;
+use hospital;
+
+drop table if exists pacientes;
+create table if not exists pacientes (
+id int not null auto_increment primary key,
+nome varchar(255) not null unique,
+preferencial boolean not null,
+numerofila int not null
+);
